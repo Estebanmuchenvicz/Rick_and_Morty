c1e409e62a0ac3773a6f23d37cb67d311283d07e
Esteban.much <esteban.muchenvicz@gmail.com>
2023-07-25 17:52:10 -0300

    about

--- Client/src/components/about/About.jsx
+++ Client/src/components/about/About.jsx
@@ -9,7 +9,13 @@ function About (){
             <div>
             <h2>Esteban Muchenvicz</h2>
             <h3>Full Stack Developer</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor soluta voluptatum blanditiis natus, omnis rerum quo, cupiditate ut harum corporis accusamus rem commodi. In id necessitatibus molestiae eius esse voluptatum!</p>
+            <p>¡Hola! Soy Esteban, un apasionado Desarrollador Web Full Stack con especial interés en el desarrollo Front End.
+
+En la última década, trabajé en el campo de "Comunicación Social" y decidí crear mi propio medio de comunicación. Durante este proceso, me adentré en el mundo de la programación para desarrollar mi propia web. Descubrí mi pasión por la tecnología y tomé la valiente decisión de hacer la transición hacia una carrera en desarrollo web.
+
+Para mejorar mis habilidades, me inscribí en Soy Henry Bootcamp, donde perfeccioné mis conocimientos en desarrollo web 🚀.
+
+🛠 Tecnologías: JavaScript | React.js | Redux | Node.js | PostgreSQL | Express.js | MongoDB | HTML5 | CSS | SCRUM | Git/GitHub</p>
             <div >
                 <li className={style.social}>
                 <a href="https://www.linkedin.com/in/esteban-muchenvicz/" target="_blank" rel="noreferrer">
